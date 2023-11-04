5d48fe1a339264243577a5e511c87f814edabc85
Maya Fouad <112335115+mayafouad@users.noreply.github.com>
2023-11-05 01:04:19 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,35 @@
+// File: main.cpp
+// Author:  Maya Fouad        Asmaa Atef        Fatma Ibrahim
+// ID:      20220270           20221020           20220242
+// TA: Mohamed Talaat Saad Farrag
+// Section: 19, 20
+// Date: 4 Nov 2023
 #include <bits/stdc++.h>
 #include "BigReal.h"
 using namespace std;
 
 int main(){
-    BigReal n1 ("11.9000000000000000000000000000000001");
-    BigReal n2 ("2333333333339.1134322222222292");
-    BigReal n3 = n1 +n2;
-    n3.print();
-    n3 = n3 - BigReal (0.9);
-    n3.print();
+    string n1; cin >> n1  ;
+    BigReal num1(n1);
+    num1.print();
+    BigReal num2("9600000002.962474100000000000000000000");
+    cout << num2;
+    BigReal num3(0.999);
+    cout << num3;
+    BigReal num4(num1);
+    BigReal num5 = num1;
+    num4.print(); num5.print();
+    BigReal num6 ;
+    num6 = num1 + num4;
+    num6.print();
+    BigReal num7 ;
+    num7 = num1 - num4;
+    num7.print();
+    BigReal num8 ;
+    num8 =  num2 + num3;
+    cout << num8;
+    cout << (num1>num3) << endl;
+    cout << (num1<num3) << endl;
+    cout << (num1==num3) << endl;
+
 }
